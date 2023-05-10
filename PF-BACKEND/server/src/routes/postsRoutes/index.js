adba7708a142ba3ad7a221a0f7fbc3ba691bcde4
salufelipe <felilanari@gmail.com>
2023-05-10 10:38:18 -0300

    ruta get post

--- PF-BACKEND/server/src/routes/postsRoutes/index.js
+++ PF-BACKEND/server/src/routes/postsRoutes/index.js
@@ -1,5 +1,17 @@
 const { Router } = require("express");
+const handlerGetPosts = require("./postsHandlers/getHandler")
 
 const api = Router();
+
+//* Trae todos los posteos de platos
+api.get("/", handlerGetPosts);
+
+//*Crear un posteo
+//? api.post("/", );
+//*Eliminar un posteo
+//? api.delete("/", );
+//*Actualizar un posteo
+//? api.put("/", );
+
 
 module.exports = api;
