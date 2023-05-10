5a10b60f6228d97ba96a5d291d002acad9c254bb
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-05-10 13:02:20 -0300

    actualizacion put users

--- PF-BACKEND/server/src/routes/usersRoutes/usersHandlers/handlerGetUsers.js
+++ PF-BACKEND/server/src/routes/usersRoutes/usersHandlers/handlerGetUsers.js
@@ -1,9 +1,11 @@
-const getUserController = require('../usersControllers/getUsersControllers')
+const getUserController = require('../usersControllers/getUsersControllers');
+const modelateData = require('../../../utils/modelateData')
 
 const handlerGetUsers = async(req,res) =>{
     try{
         const info = await getUserController();
-        res.status(200).json(info)
+        const dataModelate = modelateData(info)
+        res.status(200).json(dataModelate)
     }catch(err){
         res.staus(400).json({error:err.message});
     }
