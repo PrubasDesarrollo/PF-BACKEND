68919b8a7db1d2eef877fcfcc734a2e8a2f6fd72
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-05-11 16:08:00 -0300

    user modificado con sus rutas

--- PF-BACKEND/src/routes/usersRoutes/usersControllers/getUsersControllers.js
+++ PF-BACKEND/src/routes/usersRoutes/usersControllers/getUsersControllers.js
@@ -1,29 +1,56 @@
 const users = require('../../../db/models/Users');
 const posts = require('../../../db/models/Posts');
 const tables = require('../../../db/models/Tables');
-const modelateData = require('../../../utils/modelateData')
+const averageGrades = require('../../../utils/averageGrades')
 
-const getUserController = async(params) =>{
-
-    let infoSend
+const getUserController = async() =>{
     const tablesData = tables.find();
     const postsData = posts.find();
-    infoSend = await users.find().sort({rating: -1}).populate('posts').populate('table').exec()
+    return users.find().sort({rating: -1}).populate('posts').populate('table').exec()
+}
 
-    for(let prop in params){
-        if (prop == "order"){
-            if(params[prop]=="alphaasc"){
-                infoSend = await users.find().sort({name: 1, rating: -1}).populate('posts').populate('table').exec()
-            }
-            if(params[prop]=="alphadesc"){
-                infoSend = await users.find().sort({name: -1, rating: -1}).populate('posts').populate('table').exec();
-            }}
-        if (prop =="page"){
-            return modelateData(params[prop],infoSend)
-        }
+const getUserControllerQuery= async(order) =>{
+    
+    const tablesData = await tables.find();
+    const postsData = await posts.find();
+    const data = await users.find().sort({name:-1}).populate('posts').populate('table').exec();
+    const usersRating = await mapDataRating(data);
+    if(order == "alphadesc"){
+        return usersRating.sort((a, b) => b.name - a.name)
     }
-    // return infoSend
+    if(order == "alphaasc"){
+        return usersRating.sort((a, b) => a.name - b.name)
+    }
+    if(order == "ratingasc"){
+        return usersRating.sort((a, b) => a.rating - b.rating)
+    }
+    if(order == "ratingdesc"){
+        return usersRating.sort((a, b) => a.rating - b.rating)
+    }
+}
+
+const mapDataRating = async (data) => {
+    const mapData = data.map((user) => {
+        return{
+            id:user._id,
+            name: user.name,
+            email: user.email,
+            image: user.image,
+            type_customer: user.type_customer,
+            description: user.description,
+            valoraciones: user.valoraciones ? user.valoraciones : [0],
+            rating: averageGrades(user.valoraciones),
+            posts: user.menu,
+            table: user.table,
+        }
+    });
+    return mapData;
+
 }
 
 
-module.exports = getUserController; 
+
+module.exports = {
+    getUserController,
+    getUserControllerQuery
+} 
