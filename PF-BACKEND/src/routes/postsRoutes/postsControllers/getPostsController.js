cfd4f47900783f3b36b833e70565d69eb709c6b4
salufelipe <felilanari@gmail.com>
2023-05-11 16:43:32 -0300

    ordenamientos de posts

--- PF-BACKEND/src/routes/postsRoutes/postsControllers/getPostsController.js
+++ PF-BACKEND/src/routes/postsRoutes/postsControllers/getPostsController.js
@@ -1,12 +1,39 @@
 const posts = require('../../../db/models/Posts');
 const users = require('../../../db/models/Users');
 const restaurants = require('../../../db/models/Restaurants');
+const averageGrades = require("../../../utils/averageGrades");
 
-const getPostsController = () =>{
-    const usersData = users.find();
-    const restaurantsData = restaurants.find();
-    return posts.find().populate('authorUser').populate('authorRest').exec();
+const getPostsController = async () =>{
+    const usersData = await users.find();
+    const restaurantsData = await restaurants.find();
+    const data = posts.find().sort({rating: -1}).populate('authorUser').populate('authorRest').exec();
+    const postRatings = await mapDataRating(data);
+
+    return postRatings.sort((a, b) => b.rating - a.rating)
 }
 
+const getPostsOrdered = async (order) =>{
+    if(order === "alphdesc"){
+        return postRatings.sort((a, b) =>b.name - a.name)
+    }else if(order === "alphaasc"){
+    return postRatings.sort((a, b) => a.name - b.name)
+}   else if(order === "ratingasc"){
+    return postRatings.sort((a, b) => a.rating - b.rating)
+}
+}
 
-module.exports = getPostsController;
+const mapDataRating = async (data) => {
+    const mapData = data.map((post) => {
+        return{
+            name: post.name,
+            description: post.description,
+            ingredients: post.ingredients,
+            original: post.original,
+            cost: post.cost,
+            rating: averageGrades(post.rating),
+            authorUser: post.authorUser ? post.authorUser : "" ,
+            authorRest: post.authorRest ? post.authorRest : ""
+        }
+    })
+}
+module.exports = {getPostsController, getPostsOrdered};
--- PF-BACKEND/src/routes/postsRoutes/postsHandlers/getHandler.js
+++ PF-BACKEND/src/routes/postsRoutes/postsHandlers/getHandler.js
@@ -1,10 +1,15 @@
-const getPostsController = require("../postsControllers/getPostsController")
+const {getPostsController, getPostsOrdered} = require("../postsControllers/getPostsController")
 const modelateData = require("../../../utils/modelateData")
 
 const handlerGetPosts = async (req, res) => {
     try {
-        const {page} = req.query
-        const posts = await getPostsController();
+        const {page, order} = req.query;
+        let posts;
+        if(!order || order === "ratingdesc" ){
+        posts = await getPostsController();}
+        else{
+        posts = await getPostsOrdered(order)
+        }
         let info = modelateData(page,posts)
         res.status(200).json(info);
     } catch (error) {
