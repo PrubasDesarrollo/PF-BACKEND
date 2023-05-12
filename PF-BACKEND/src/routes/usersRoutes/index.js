e92cd869ba8a03d338e27da97ef72ec604ca913f
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-05-12 18:36:49 -0300

    post user con image

--- PF-BACKEND/src/routes/usersRoutes/index.js
+++ PF-BACKEND/src/routes/usersRoutes/index.js
@@ -1,4 +1,6 @@
 const { Router } = require("express");
+const uploadImage = require('../../services/firebase')
+const multer = require("multer");
 const handlerGetUsers = require('./usersHandlers/handlerGetUsers')
 const handlerPostUser = require('./usersHandlers/handlerPostUser')
 const handlerDeleteUser = require('./usersHandlers/handlerDeleteUser')
@@ -7,11 +9,16 @@ const handlerGetUser = require('./usersHandlers/handlerGetUser')
 
 const api = Router();
 
+const Multer = multer({
+  storage: multer.memoryStorage(),
+  limits: 1024 * 1024,
+});
+
 api.get("/", handlerGetUsers);
 
 api.get("/:id", handlerGetUser)
 
-api.post("/", handlerPostUser);
+api.post("/", Multer.single("image"), uploadImage, handlerPostUser);
 
 api.delete("/:id", handlerDeleteUser);
 
