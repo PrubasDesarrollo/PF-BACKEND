019af1c41115d1025af49bdc6ca77e4ca0611d00
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-05-12 19:06:15 -0300

    multer en app

--- PF-BACKEND/src/routes/postsRoutes/postsHandlers/postHandlerPosts.js
+++ PF-BACKEND/src/routes/postsRoutes/postsHandlers/postHandlerPosts.js
@@ -2,8 +2,9 @@ const controllerPostPosts = require("../postsControllers/PostPostsController");
 
 const handlerPostPosts = async (req, res) =>{
     try {
+        const { firebaseUrl } = req.file ? req.file : "";
         const data = req.body;
-        let info = await controllerPostPosts(data);
+        let info = await controllerPostPosts(data, firebaseUrl);
         res.status(200).json(info);
     } catch (err) {
         res.status(400).json({error:err.message})
