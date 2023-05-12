886a2f9a02df36d1237b08f5312b2132675c5296
victor <victor.lavado15@hotmail.com>
2023-05-12 16:02:05 -0500

    listo el link en la prop image del Users

--- PF-BACKEND/src/db/models/Users.js
+++ PF-BACKEND/src/db/models/Users.js
@@ -1,38 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const usersScheme = new mongoose.Schema(
-    {
-        name:{
-            type: String
-        },
-        email:{
-            type: String
-        },
-        image:{
-            type: Buffer
-        },
-        type_customer:{
-            type: String
-        },
-        description:{
-            type: String
-        },
-        valoraciones:{
-            type: Array
-        },
-        rating:{
-            type: Number
-        },
-        posts:{
-            type:[ mongoose.Schema.Types.ObjectId], ref:'posts'
-        },
-        table:{
-            type: mongoose.Schema.Types.ObjectId, ref:'tables'
-        }
+  {
+    name: {
+      type: String,
     },
-    {
-        versionKey: false,
-    }
-)
+    email: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    type_customer: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    valoraciones: {
+      type: Array,
+    },
+    rating: {
+      type: Number,
+    },
+    posts: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "posts",
+    },
+    table: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "tables",
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
 
-module.exports = mongoose.model('users',usersScheme)
+module.exports = mongoose.model("users", usersScheme);
---
+++ PF-BACKEND/src/services/firebase.js
@@ -0,0 +1,44 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../db/firebase config/firebaseConfig.json");
+
+const BUCKET = "pf-backend-93476.appspot.com";
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: BUCKET,
+});
+
+const bucket = admin.storage().bucket();
+
+const uploadImage = (req, res, next) => {
+  if (!req.file) return next();
+
+  const image = req.file;
+  const nombreArchivo = Date.now() + "." + image.originalname.split(".").pop();
+
+  const file = bucket.file(nombreArchivo);
+
+  const stream = file.createWriteStream({
+    metadata: {
+      contentType: image.mimetype,
+    },
+  });
+
+  stream.on("error", (e) => {
+    console.log(e);
+  });
+
+  stream.on("finish", async (e) => {
+    await file.makePublic();
+    console.log(e);
+
+    req.file.firebaseUrl = `https://storage.googleapis.com/${BUCKET}/${nombreArchivo}`;
+
+    next();
+  });
+
+  stream.end(image.buffer);
+};
+
+module.exports = uploadImage;
