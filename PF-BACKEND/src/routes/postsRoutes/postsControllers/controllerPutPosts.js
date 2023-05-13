eb7e98aa84018a960f053c94430f70724a44e854
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-05-13 11:54:42 -0300

    arreglo bugs

--- PF-BACKEND/src/routes/postsRoutes/postsControllers/controllerPutPosts.js
+++ PF-BACKEND/src/routes/postsRoutes/postsControllers/controllerPutPosts.js
@@ -1,30 +1,32 @@
 const posts = require("../../../db/models/Posts");
 const parseId = require("../../../utils/parseId");
-const getIdPostController = require("../postsControllers/getIdPostController");
 
-const controllerPutPosts = async (id, info,firebaseUrl) =>{
-    let virula;    
-    const idParsed = parseId(id)
-    for(prop in info){
-            if(prop === "rating"){
-                virula = await posts.findOneAndUpdate(idParsed, {$push: info.rating});
-            }else if(prop === "tags"){
-                virula = await posts.findOneAndUpdate(idParsed, {$addToSet: info.tags})
-            }else{
-             virula = await posts.findOneAndUpdate(idParsed,{
-                name: info.name,
-                description: info.description,
-                image: firebaseUrl,
-                ingredients: info.ingredients,
-                original: info.original,
-                cost: info.cost,
-                authorUser: info.authorUser,
-                authorRest: info.authorRest 
-            });
-        }}
-    
-    
-    return getIdPostController(idParsed);
+const controllerPutRating = (id, rating) =>{
+    let idParsed = parseId(id)
+    return posts.findOneAndUpdate(idParsed, {$push: {rating: rating}})
 }
 
-module.exports = controllerPutPosts;
+const controllerPutTags = (id, tags) =>{
+    let idParsed = parseId(id)
+    return posts.findOneAndUpdate(idParsed, {$push: {tags: tags}})
+}
+
+const controllerPutPosts = (id, info,firebaseUrl) =>{
+    let idParsed = parseId(id)
+    return posts.findOneAndUpdate(idParsed,{
+            name: info.name,
+            description: info.description,
+            image: firebaseUrl,
+            ingredients: info.ingredients,
+            original: info.original,
+            cost: info.cost,
+            authorUser: info.authorUser,
+            authorRest: info.authorRest 
+            });
+    
+}
+
+module.exports = {
+    controllerPutTags,
+    controllerPutRating,
+    controllerPutPosts};
