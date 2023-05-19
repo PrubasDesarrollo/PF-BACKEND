aa0d526ff7fc50fe3ed5c20197ea4ada2f185077
victor <victor.lavado15@hotmail.com>
2023-05-19 06:28:01 -0500

    añadido mercado pago

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,6 @@
 const { Router } = require("express");
+const mercadopago = require("mercadopago");
+require("dotenv").config();
 
 const usersRoutes = require("./usersRoutes/index");
 const postsRoutes = require("./postsRoutes/index");
@@ -13,5 +15,35 @@ router.use("/tables", tablesRoutes);
 router.use("/posts", postsRoutes);
 router.use("/restaurants", restaurantsRoutes);
 router.use("/upload", uploadRoutes);
+
+mercadopago.configure({ access_token: process.env.MERCADOPAGO_KEY });
+router.post("/payment", (req, res) => {
+  const prod = req.body;
+  let preference = {
+    items: [
+      {
+        id: 123,
+        title: prod.name,
+        currency_id: "USD",
+        picture_url: prod.image,
+        description: prod.description,
+        category_id: "art",
+        quantity: 1,
+        unit_price: prod.cost,
+      },
+    ],
+    back_urls: {
+      success: "http://localhost:3000",
+      failure: "",
+      pending: "",
+    },
+    auto_return: "approved",
+    binary_mode: true,
+  };
+  mercadopago.preferences
+    .create(preference)
+    .then((response) => res.status(200).send({ response }))
+    .catch((error) => res.status(400).send({ error: error.message }));
+});
 
 module.exports = router;
