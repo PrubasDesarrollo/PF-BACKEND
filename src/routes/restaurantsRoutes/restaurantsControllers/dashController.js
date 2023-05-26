627dae2a7b4e8a6f8e0b3facd0ed216e123460cf
Miguel Sierra <sierrabolanosmiguel@gmail.com>
2023-05-25 19:50:57 -0500

    Modelaje de datos funcionales dashboards

--- src/routes/restaurantsRoutes/restaurantsControllers/dashController.js
+++ src/routes/restaurantsRoutes/restaurantsControllers/dashController.js
@@ -11,9 +11,26 @@ const deliverysData = async (id) => {
     const restaurantsData = restaurants.find();
     const postData = posts.find();
 
-    return await delivery.find({restaurant:id}).populate('customer').populate('c_order').populate('restaurant').exec();
+    return await delivery.find({restaurant:id}).populate('c_order').populate('restaurant').exec();
 }
 
+const userDataId = async (id, cantidad) => {
+    const usersData = await users.find({_id:id},{
+        _id:0,
+        isAdmin:0,
+        type_customer:0,
+        description:0,
+        valoraciones:0,
+        created:0,
+        phone:0,
+        rating:0,
+        posts:0,
+        table:0,
+        isActive:0
+    });
+
+    return usersData;
+}
 
 const dashController = async (id) => {
     const idParsed = parseId(id)
@@ -67,12 +84,33 @@ const dashController = async (id) => {
         deliverysPerMonth.push(deliverys)
     }
 
+    const topUsers = {};
+    
+    for(let y = 0; y < deliverysStock.length; y++){
+        const user = deliverysStock[y].customer;
+        if(topUsers[user]){
+            topUsers[user]++
+        }else{
+            topUsers[user] = 1 ;
+        }
+    }
+
+    const datosUsers = Object.entries(topUsers).sort((a, b) => b[1] - a[1]).slice(0, 3);
+    const topFive = datosUsers.map(([id, cantidad]) => ({id, cantidad}))
+    const arr = [];
+
+    await Promise.all(topFive.map(async (user) => {
+        let data_user = await userDataId(user.id, user.cantidad);
+        arr.push(data_user);
+    }));
+
     data = {
         ratingPerMonth:{
             stockPerMonth,
             ratingPerMonth
         },
-        deliverysPerMonth
+        deliverysPerMonth,
+        topFiveUsers: arr.flat()
     }
 
 
