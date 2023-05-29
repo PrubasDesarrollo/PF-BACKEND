61215238938659e876c83180091df574f6647575
victor <victor.lavado15@hotmail.com>
2023-05-29 12:43:40 -0500

    actualizado para recibir varias img y eliminando cosas que no servian

--- src/routes/usersRoutes/usersControllers/controllerPostUser.js
+++ src/routes/usersRoutes/usersControllers/controllerPostUser.js
@@ -1,47 +1,53 @@
 const users = require("../../../db/models/Users");
-const banned = require('../../../db/models/Banned')
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-const { TOKEN_KEY, PASSWORD_ADMIN } = process.env
-const sendEmail = require('../../../utils/configEmailer')
+const banned = require("../../../db/models/Banned");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const { TOKEN_KEY, PASSWORD_ADMIN } = process.env;
+const sendEmail = require("../../../utils/configEmailer");
 
+const controllerPostUser = async (data, firebaseUrls, password) => {
+  let validate = await banned.find({ "user_banned.email": data.email });
+  if (validate.length !== 0) {
+    throw new Error("Usuario baneado");
+  }
 
-const  controllerPostUser = async(data, firebaseUrl,password) =>{
-    let validate = await banned.find({'user_banned.email':data.email})
-    if (validate.length!==0){throw new Error('ususario baneado')}
-    let user = {
-        name: data.name,
-        email: data.email,
-        isAdmin:password?(password===PASSWORD_ADMIN):false,
-        image:firebaseUrl,
-        type_customer: data.type_customer,
-        description: data.description
+  let user = {
+    name: data.name,
+    email: data.email,
+    isAdmin: password ? password === PASSWORD_ADMIN : false,
+    images: firebaseUrls, // Utilizando el array de URLs de las imágenes subidas
+    type_customer: data.type_customer,
+    description: data.description,
+  };
+
+  let informacion = await users.create(user);
+
+  const token = jwt.sign(
+    {
+      _id: informacion._id,
+      email: informacion.email,
+      type_customer: informacion.type_customer,
+      isAdmin: informacion.isAdmin,
+    },
+    TOKEN_KEY,
+    {
+      expiresIn: "7d",
     }
-    let informacion = await users.create(user);
-    const token = jwt.sign(
-        {
-            _id: informacion._id, 
-            email: informacion.email,
-            type_customer: informacion.type_customer,
-            isAdmin: informacion.isAdmin
-        },
-        TOKEN_KEY,
-        {
-            expiresIn: "7d" 
-        }
-    )
-    let infoUser = {
-        isAdmin: informacion.isAdmin,
-        name: informacion.name,
-        email: informacion.email,
-        image:informacion.image,
-        type_customer: informacion.type_customer,
-        description: informacion.description,
-        token: token,
-        id: informacion._id
-    }
-    await sendEmail(data.email);
-    return infoUser;
-}
+  );
+
+  let infoUser = {
+    isAdmin: informacion.isAdmin,
+    name: informacion.name,
+    email: informacion.email,
+    images: informacion.images, // Utilizando el array de URLs de las imágenes guardadas
+    type_customer: informacion.type_customer,
+    description: informacion.description,
+    token: token,
+    id: informacion._id,
+  };
+
+  await sendEmail(data.email);
+  return infoUser;
+};
 
 module.exports = controllerPostUser;
--- src/routes/usersRoutes/usersHandlers/handlerPostUser.js
+++ src/routes/usersRoutes/usersHandlers/handlerPostUser.js
@@ -1,16 +1,15 @@
-const controllerPostUser = require('../usersControllers/controllerPostUser')
+const controllerPostUser = require("../usersControllers/controllerPostUser");
 
-
-const handlerPostUser = async(req,res) =>{
-    const { firebaseUrl } = req.file ? req.file : "";
-    const data = req.body;
-    const {password} = req.query
-    try{
-        let info = await controllerPostUser(data,firebaseUrl,password);
-        res.status(200).json(info)
-    }catch(err){
-        res.status(400).json({error:err.message})
-    }
-}
+const handlerPostUser = async (req, res) => {
+  const firebaseUrls = req.files.map((file) => file.firebaseUrl);
+  const data = req.body;
+  const { password } = req.query;
+  try {
+    let info = await controllerPostUser(data, firebaseUrls, password);
+    res.status(200).json(info);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
 
 module.exports = handlerPostUser;
