d6bd88144787704ccdcf6e12b20e1151d2c447dc
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-05-30 18:53:03 -0300

    calculateCapacity

---
+++ src/utils/calculateCapacity.js
@@ -0,0 +1,16 @@
+const calculateCapacity = (totalQuantity, arr, obj) =>{
+    let totalPeople = 0;
+    arr.map((reserv)=>{
+        totalPeople += reserv.people
+    })
+    let compared = totalPeople + obj.people
+
+    if (compared > totalQuantity){
+        let remainingCapacity = totalQuantity - totalPeople;
+        throw new Error(`Exceeds the total capacity of the restaurant, space available: ${remainingCapacity} people`)
+    }
+    else{
+        return obj
+    }
+
+}
