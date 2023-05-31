c09b999480a6625252eeaac0431bf4b3150ab2c6
SantiagoLeyriaLino <leyrialinosantiago@gmail.com>
2023-05-30 23:30:29 -0300

    reservas terminadas papu

--- src/utils/calculateCapacity.js
+++ src/utils/calculateCapacity.js
@@ -1,4 +1,10 @@
-const calculateCapacity = (totalQuantity, arr, obj) => {
+const users = require('../db/models/Users');
+const restaurants = require('../db/models/Restaurants')
+
+const calculateCapacity = async(totalQuantity, arr, obj, idRestaurant) => {
+  const idUser = obj.nombre
+  const user = await users.findById(obj.nombre,'name phone email _id').exec() 
+  const restaurant = await restaurants.findById(idRestaurant,'name phone email address _id').exec() 
   let totalPeople = 0;
   arr.forEach((reserv) => {
     totalPeople += reserv.people;
@@ -11,7 +17,18 @@ const calculateCapacity = (totalQuantity, arr, obj) => {
       `Exceeds the total capacity of the restaurant, space available: ${remainingCapacity} people`
     );
   } else {
-    return obj;
+    let objUserReserved = {
+      ...obj,
+      nombre: restaurant
+    }
+
+    await users.findOneAndUpdate({_id: idUser}, {reservation:objUserReserved})
+
+    let newObj = {
+      ...obj,
+      nombre: user
+    }
+    return newObj;
   }
 };
 
