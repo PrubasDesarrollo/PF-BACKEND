f6ec3776c18c1cfb6c3efcf678f47f9994dc613e
victor <victor.lavado15@hotmail.com>
2023-05-30 19:03:18 -0500

    arreglado algunos bug

--- src/routes/restaurantsRoutes/restaurantsControllers/getReservationsController.js
+++ src/routes/restaurantsRoutes/restaurantsControllers/getReservationsController.js
@@ -1,18 +1,17 @@
 const parseId = require("../../../utils/parseId");
 const restaurants = require("../../../db/models/Restaurants");
-const users = require("../../../db/models/Restaurants");
+const users = require("../../../db/models/Users");
 
 const getReservationsController = async (id) => {
-    const idParsed = parseId(id);
-    const usersData = await users.find();
+  const idParsed = parseId(id);
+  const usersData = await users.find();
 
-    const reservations = await restaurants.find({_id: idParsed}, {"reservations":1})
-                                          .populate({path: "reservations", populate: {path: "nombre"}})
-                                          .exec();
-    
-    return reservations.shift();
-}
+  const reservations = await restaurants
+    .find({ _id: idParsed }, { reservations: 1 })
+    .populate({ path: "reservations", select: "nombre" })
+    .exec();
 
-
+  return reservations.shift();
+};
 
 module.exports = getReservationsController;
